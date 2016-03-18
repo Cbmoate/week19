db3f343708586d50853d13cb3202a9d379ea6fdd
CB <cbmoate@gmail.com>
2016-03-18 17:03:21 -0400

    added starter HTML

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyparser = require('body-parser');
 
 var PORT = process.env.PORT || 9001;
 
+var mongoose = require('mongoose');
+
 app.listen(PORT, function(){
   console.log("Goliath online port", PORT);
 });
