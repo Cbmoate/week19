0a2cd0bba5ba7c19928328ca2aaeca8cf8c428d1
CB <cbmoate@gmail.com>
2016-03-21 15:15:08 -0400

    updates to serving

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var logger = require('morgan');
 
