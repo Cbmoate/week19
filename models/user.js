220508f2ac1439b4020e73de0948ab3acfaf6740
CB <cbmoate@gmail.com>
2016-03-21 11:34:42 -0400

    added schema for comments, items, users

---
+++ models/user.js
@@ -0,0 +1,10 @@
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+ money: Number,
+ itemsOwned: String,
+ 
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports= User;
